57b53c4e8aedcc90aa1ce4aea9042bc938a6300d
Unknown <adtsolanki22@gmail.com>
2022-09-07 16:56:56 +0530

    Display All events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,11 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
 function EventPage() {
+   const allEvents = getAllEvents();
    return (
-      <div>Event Page</div>
+      <div>
+         <EventList events={allEvents}/>
+      </div>
    )
 }
 export default EventPage;
