b17f2d1c8d3a4bf0ede519b2bc548c196334c437
Unknown <adtsolanki22@gmail.com>
2022-09-07 19:48:14 +0530

    Adding static site generation(SSG) on Home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
-import { getFeaturedEvents } from '../dummy-data'
+//import { getFeaturedEvents } from '../dummy-data'
+import { getFeaturedEvents } from "../helpers/api-util"
 import EventList from "../components/events/event-list"
-export default function HomePage() {
-  const featuredEvent = getFeaturedEvents();
+function HomePage(props) {
+  //const featuredEvent = getFeaturedEvents();
   return (
     <div>
-      <EventList events={featuredEvent}/>
+      {/* <EventList events={featuredEvent}/> */}
+      <EventList events={props.events}/>
     </div>
   )
 }
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents
+    }
+  }
+}
+export default HomePage;
