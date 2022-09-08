1af3411937db7f1f8f1bf6dc31d61187adaf9a8f
Unknown <adtsolanki22@gmail.com>
2022-09-08 13:16:46 +0530

    Adding head content

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 //import { getFeaturedEvents } from '../dummy-data'
+import Head from "next/head";
 import { getFeaturedEvents } from "../helpers/api-util"
 import EventList from "../components/events/event-list"
 function HomePage(props) {
   //const featuredEvent = getFeaturedEvents();
   return (
     <div>
+      {/* add html section,nextjs inject them into a head section */}
+      <Head>
+        <title>NextJS Events</title>
+        Meta is show up in serach result when your page
+        <meta 
+          name="description" 
+          content="Find a lot of great events that allow you to evolve..."
+          />
+      </Head>
       {/* <EventList events={featuredEvent}/> */}
       <EventList events={props.events}/>
     </div>
