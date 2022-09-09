e963de25f43efbb844a54e206aa986f4e04cbf9a
Unknown <adtsolanki22@gmail.com>
2022-09-09 17:05:38 +0530

    Insert comment and newsletter into database and fetch comments

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,9 +1,15 @@
  // /api/comment/some event id
- function handler(req,res) {
+ import { MongoClient } from"mongodb";
+ 
+ async function handler(req,res) {
     const eventId = req.query.eventId;
     console.log("event id is",eventId);
     console.log("req body is",req.body)
-   if(req.method === 'POST') {
+
+    const MONGO_URI = `mongodb+srv://aditi:NruNqUTV1OPMxTvN@cluster0.v364j.mongodb.net/eventsApp?retryWrites=true&w=majority`;
+    const client = await MongoClient.connect(MONGO_URI);
+
+    if(req.method === 'POST') {
       //add server-side validation
       const { email, name, text } = req.body;
       if(!email.includes("@") || !name || name.trim() === '' || !text || text.trim() === '') {
@@ -15,17 +21,28 @@
          id: new Date().toISOString(),
          email: email,
          name: name,
-         text: text
+         text: text,
+         eventId: eventId
       }
       console.log("new comment is",newComment);
+      const db = client.db();
+      const result = await db.collection("comments").insertOne(newComment);
+      console.log("Result is",result);
+      newComment.id = result.insertedId;
       res.status(200).json({ message: "Added Comment.", comment: newComment })
    }
    if(req.method === 'GET') {
-      const dummyComment = [
-         {id: "c1", name: "Aditi", text: "First Comment"},
-         {id: "c2", name: "Arjun", text: "Second Comment"}
-      ];
-      res.status(200).json({ comments: dummyComment });
+      // const dummyComment = [
+      //    {id: "c1", name: "Aditi", text: "First Comment"},
+      //    {id: "c2", name: "Arjun", text: "Second Comment"}
+      // ]; const db = client.db();
+      const db = client.db();
+      const result = await db.collection("comments").find().sort().toArray();
+      console.log("result is",result)
+      res.status(200).json({ comments: result });
    }
+
+   client.close();
+ 
  }
  export default handler;
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -7,15 +7,15 @@ async function handler(req, res) {
          res.status(422).json({ message: "Invalid email address" });
          return;
       }
-      
+
    //       "mongoDatabase": "lyricaldb",
    //       "mongoUserName": "aditi",
    //       "mongoUserPassword": "NruNqUTV1OPMxTvN"
-      const MONGO_URI = `mongodb+srv://aditi:NruNqUTV1OPMxTvN@cluster0.v364j.mongodb.net/newsletter?retryWrites=true&w=majority`;
+      const MONGO_URI = `mongodb+srv://aditi:NruNqUTV1OPMxTvN@cluster0.v364j.mongodb.net/eventsApp?retryWrites=true&w=majority`;
 
       const client = await MongoClient.connect(MONGO_URI)
       const db = client.db();
-      await db.collection("emails").insertOne({ email: userEmail });
+      await db.collection("newsletter").insertOne({ email: userEmail });
 
       client.close();
       console.log("email is",userEmail)
