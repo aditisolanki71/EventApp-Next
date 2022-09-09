db90020d7e598e528c957bfcab21ce16eb62fd58
Unknown <adtsolanki22@gmail.com>
2022-09-09 14:29:06 +0530

    Added CommentList and NewsletterRegistration

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -8,6 +8,7 @@ import EventSummary from "../../components/event-detail/event-summary"
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import ErrorAlert from "../../components/ui/error-alert";
+import Comments from "../../components/input/comments";
 function EventDetailPage(props) {
    console.log("event detail page called",props)
    // const router = useRouter();
@@ -37,6 +38,7 @@ function EventDetailPage(props) {
          <EventContent>
             <p>{event.description}</p>
          </EventContent>
+         <Comments eventId={event.id} />
       </Fragment>
    )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 import Head from "next/head";
 import { getFeaturedEvents } from "../helpers/api-util"
 import EventList from "../components/events/event-list"
+import NewsletterRegistration from "../components/input/newsletter-registration"
 function HomePage(props) {
   //const featuredEvent = getFeaturedEvents();
   return (
@@ -15,6 +16,7 @@ function HomePage(props) {
           content="Find a lot of great events that allow you to evolve..."
           />
       </Head>
+      <NewsletterRegistration />
       {/* <EventList events={featuredEvent}/> */}
       <EventList events={props.events}/>
     </div>
